ef485ea5029b0ec3a5beed907f2c49e024562912
Jim Thomason <jim@jimandkoka.com>
2013-02-22 20:45:16 -0600

    can now collapse and rewire the height of the browser simultaneously

--- Iris/jquery/kbase/kbaseIrisCommands.js
+++ Iris/jquery/kbase/kbaseIrisCommands.js
@@ -14,6 +14,8 @@
             },
             englishCommands : 0,
             fontSize : '75%',
+            overflow : true,
+            sectionHeight : '300px',
         },
 
         init: function(options) {
@@ -46,6 +48,57 @@
             return completions;
         },
 
+        commonPrefix : function(str1, str2) {
+
+            var prefix = '';
+            for (var idx = 0; idx < str1.length && idx < str2.length; idx++) {
+                var chr1 = str1.charAt(idx);
+                var chr2 = str2.charAt(idx);
+                if (chr1 == chr2) {
+                    prefix = prefix + chr1;
+                }
+                else {
+                    break;
+                }
+            };
+
+            return prefix;
+        },
+
+        comonCommandPrefix : function (commands) {
+
+            var prefix = '';
+
+            if (commands.length > 1) {
+
+            //find the longest common prefix for the first two commands. That's our start.
+                prefix = this.commonPrefix(commands[0], commands[1]);
+
+                for (var idx = 2; idx < commands.length; idx++) {
+                    prefix = this.commonPrefix(prefix, commands[idx]);
+                }
+
+            }
+            else {
+                prefix = commands[0];
+            }
+
+            return prefix;
+
+        },
+
+        commandsMatchingRegex : function (regex) {
+            console.log(regex);
+            var matches =[];
+            for (var idx = 0; idx < this.commands.length; idx++) {
+                if (this.commands[idx].match(regex)) {
+                    matches.push(this.commands[idx]);
+                }
+            }
+
+            return matches.sort();
+        },
+
 
         appendUI : function($elem) {
             this.client.valid_commands_async(
@@ -61,8 +114,8 @@
 
                                     var $ul = $('<ul></ul>')
                                         .addClass('unstyled')
-                                        .css('max-height', '300px')
-                                        .css('overflow', 'auto')
+                                        .css('max-height', this.options.overflow ? this.options.sectionHeight : '5000px')
+                                        .css('overflow', this.options.overflow ? 'auto' : 'visible')
                                     ;
 
                                     $.each(
@@ -82,34 +135,7 @@
                                                 this.commands.push(val.cmd);
 
                                                 $ul.append(
-                                                    $('<li></li>')
-                                                        .append($('<a></a>')
-                                                            .attr('href', '#')
-                                                            .attr('title', val.cmd)
-                                                            .data('blockType', 'narrativeBlock')
-                                                            .css('display', 'list-item')
-                                                            //.tooltip()
-                                                            .text(label)
-                                                            .bind(
-                                                                'click',
-                                                                this.options.link
-                                                            )
-                                                        )
-                                                        /*.draggable(
-                                                            {
-                                                                distance : 20,
-                                                                cursor   : 'pointer',
-                                                                opacity  : 0.7,
-                                                                helper   : 'clone',
-                                                                connectToSortable: this.options.connectToSortable,
-                                                                revert : 'invalid',
-                                                                disabled : this.options.connectToSortable == undefined,
-                                                                cursorAt : {
-                                                                    left : 5,
-                                                                    top  : 5
-                                                                }
-                                                            }
-                                                        )*/
+                                                    this.createLI(val.cmd, label)
                                                 );
                                             },
                                             this
@@ -135,7 +161,45 @@
 
         },
 
+        createLI : function(cmd, label) {
+
+            if (label == undefined) {
+                label = cmd;
+            }
+
+            return $('<li></li>')
+                .append($('<a></a>')
+                    .attr('href', '#')
+                    .attr('title', cmd)
+                    .data('blockType', 'narrativeBlock')
+                    .css('display', 'list-item')
+                    //.tooltip()
+                    .text(label)
+                    .bind(
+                        'click',
+                        this.options.link
+                    )
+                )
+                /*.draggable(
+                    {
+                        distance : 20,
+                        cursor   : 'pointer',
+                        opacity  : 0.7,
+                        helper   : 'clone',
+                        connectToSortable: this.options.connectToSortable,
+                        revert : 'invalid',
+                        disabled : this.options.connectToSortable == undefined,
+                        cursorAt : {
+                            left : 5,
+                            top  : 5
+                        }
+                    }
+                )*/
+        },
+
         loadedCallback : function($elem, commands) {
+
+            var that = this;
 
             var $div = $('<div></div>')
                 .css('border', '1px solid lightgray')
@@ -146,11 +210,27 @@
                         .text("Command List")
                         .css('background-color', 'lightgray')
                         .css('padding', '2px')
-                        .css('border-collapse', 'collapse')
                         .css('margin', '0px')
-                    )
+                        .bind('click',
+                            function(e) {
+                                $(this).next().collapse('toggle');
+                                if (that.options.fileBrowser) {
+                                    that.options.fileBrowser.toggleNavHeight();
+                                }
+                            }
+                        )
+                )
+                .append(
+                    $('<ul></ul>')
+                        .css('font-size', this.options.fontSize)
+                        .css('padding-left', '15px')
+                        .attr('id', 'searchResults')
+                        .addClass('unstyled')
+                )
             ;
             $elem.append($div);
+
+            this._rewireIds($div, this);
 
             this._superMethod('appendUI', $div, commands);
 
