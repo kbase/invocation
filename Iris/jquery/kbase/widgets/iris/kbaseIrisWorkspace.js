b1d78c7257713eaa43871ce6453f3a779189164d
Jim Thomason <thomason@cshl.edu>
2014-02-06 16:30:29 -0600

    rebranded back to IRIS

--- Iris/jquery/kbase/widgets/iris/kbaseIrisWorkspace.js
+++ Iris/jquery/kbase/widgets/iris/kbaseIrisWorkspace.js
@@ -44,7 +44,7 @@
                                             e.stopPropagation();
                                             this.addTab(
                                                 {
-                                                    tab : 'Iris Terminal',
+                                                    tab : 'IRIS Terminal',
                                                     content : $term.$elem,
                                                     canDelete : false,
                                                     show : true,
